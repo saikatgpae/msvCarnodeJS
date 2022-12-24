ef51d43897dd97c3bd8cb3b0ef9da756abe9985b
Saikat Chakraborty <saikatgpae@gmail.com>
2022-12-24 19:14:51 +0530

    Add email sender.

---
+++ sendMail.js
@@ -0,0 +1,28 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'saikatgpae@gmail.com',
+    pass: ''
+  }
+});
+
+var mailOptions = {
+  from: 'saikatgpae@gmail.com',
+  to: 'saikatgpae@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+
+// user:'saikatgpae@gmail.com',
+//         pass:'Indra1911@#'
